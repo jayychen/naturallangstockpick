eb29fa7a709d04a0739ca9e84c024182d56aedb0
ts2 <ychen@ts2>
2023-04-24 12:01:09 -0500

    disclaimer touchscreen

--- web/public/main.js
+++ web/public/main.js
@@ -5,6 +5,7 @@ const exploreButton = document.getElementById('explore');
 const responseTable = document.getElementById('responseTable');
 const errorContainer = document.getElementById('errorContainer');
 const infoContainer = document.getElementById('infoContainer');
+const disclaimer = document.getElementById('disclaimer');
 
 submitQuestion.addEventListener('click', () => {
   const question = questionInput.value;
@@ -58,4 +59,8 @@ socket.on('response', (response) => {
     });
     responseTable.appendChild(tableBody);
   }
+});
+
+disclaimer.addEventListener('click', () => {
+  infoContainer.innerHTML = `<div class="alert alert-secondary">DISCLAIMER: The information provided on this website is for educational and informational purposes only and should not be considered as investment advice. The website does not make any representation or warranty as to the accuracy, completeness, or timeliness of the information on this site. Any reliance you place on such information is therefore strictly at your own risk. The website shall not be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.</div>`;
 });
